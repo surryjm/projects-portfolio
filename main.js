ad791d489f24eb89f6162c5db8940cc81e90477c
Surry Mowery <surrykj@gmail.com>
2022-04-24 10:46:00 -0700

    menu updates in main.js

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
+(function() {
+
+const nav = document.getElementById('navbar');
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const logo = document.getElementById('logo');
+const navLinks = document.querySelectorAll('.nav-links');
+
+// Open responsive navigation
 function openNav() {
-  let nav = document.getElementById('navbar');
-  console.log(nav);
+  let main = document.getElementById('main');
   if (nav.className === 'navbar') {
-    nav.className += ' responsive';
+    nav.className += ' nav-open';
+    main.className = 'blur';
   } else {
     nav.className = 'navbar';
+    main.classList.remove('blur');
   }
 }
+
+// Close responsive navigation
+function closeNav() {
+  let nav = document.getElementById('navbar');
+  let main = document.getElementById('main');
+  if (nav.classList.contains('nav-open')) {
+    nav.className = 'navbar';
+    main.classList.remove('blur');
+  }
+}
+
+// Add event listeners
+hamburgerIcon.addEventListener('click', openNav);
+logo.addEventListener('click', closeNav);
+navLinks.forEach(navlink => {
+  navlink.addEventListener('click', closeNav);
+});
+
+// Close responsive navigation if window is > 768px
+window.addEventListener('resize', function() {
+  let nav = document.getElementById('navbar');
+  let main = document.getElementById('main');
+  if (window.innerWidth > 768) {
+    nav.className = 'navbar';
+    main.classList.remove('blur');
+  }
+});
+
+})();
